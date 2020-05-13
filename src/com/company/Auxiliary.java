9c9b639b4dd5e48b78c76281be0175bd3f26cb70
Anton <sogonyaevjk@mail.ru>
2020-05-13 13:17:26 +0300

    next12

--- src/com/company/Auxiliary.java
+++ src/com/company/Auxiliary.java
@@ -6,8 +6,61 @@ import java.util.Random;
 public class Auxiliary {
 
 
+    public static String returnName(String name, int id) {
+        int transferForName = 5 + (int) (id / 1000);
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        String generatedString = null;
 
-    public static String returnName(String name) {
+        for (int x = 1; x <= 100; x++) {
+            StringBuilder buffer = new StringBuilder(targetStringLength);
+            for (int i = 0; i < transferForName; i++) {
+                int randomLimitedInt = leftLimit + (int)
+                        (transferForName * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            generatedString = buffer.toString();
+
+        }
+
+        return generatedString;
+    }
+
+
+    public static int forZp(int id) {
+        int z = (int) (id / 10);
+        return z;
+    }
+
+    public static int forTrans() {
+        int z = 10 + (int) (Math.random() * 99);
+        return z;
+    }
+
+
+    public static String forDate(int id) {
+        int r = id;
+
+        int z = 0;
+        int y = 0;
+        int x = 0;
+
+
+        z = id%100 + 1980;
+        x = id%12 + 1;
+        y = id%30;
+
+
+
+        String dateBirthday = new String(z + "." + x + "." + y);
+        return dateBirthday;
+
+
+    }
+}
+    /*public static String returnName(String name) {
         int transferForName = 5 + (int) (Math.random() * 10);
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
@@ -27,27 +80,4 @@ public class Auxiliary {
         }
 
         return generatedString;
-    }
-
-
-    public static  int forZp(int id){
-       int z = (int) (id/10);
-       return z;
-    }
-
-    public static  int forTrans(int id){
-        int z = (int) (id/100);;
-        return z;
-    }
-
-
-    public static String forDate(){
-        int z =1970 + (int) (Math.random() * 40);
-        int x =1 + (int) (Math.random() * 12);
-        int y =1 + (int) (Math.random() * 30);
-
-        String dateBirthday = new String(z+"."+x+"."+y);
-        return dateBirthday;
-    }
-
-}
+    }*/
--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -18,23 +18,46 @@ class Client {
 
     public Client(int id) {
         this.id = id;
-        this.name = Auxiliary.returnName(name);
+        this.name = Auxiliary.returnName(name, this.id);
         this.zp =  Auxiliary.forZp(this.id);
-        this.transfer = Auxiliary.forTrans(this.id);
-        this.dateBirthday = Auxiliary.forDate();
+        this.transfer = Auxiliary.forTrans();
+        this.dateBirthday = Auxiliary.forDate(this.id);
     }
 
 
         public static void main(String[]args){
         Client cl = new Client(3007);
-            System.out.println(cl.zp);
+            System.out.println(cl.dateBirthday);
 
 
         Client c2 = new Client(3007);
-            System.out.println(c2.zp);
+            System.out.println(c2.dateBirthday);
 
 }
         
         
     }
+
+
+   /* public static String returnName(String name) {
+        int transferForName = 5 + (int) (Math.random() * 10);
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        String generatedString = null;
+
+        for (int x = 1; x <= 100; x++) {
+            StringBuilder buffer = new StringBuilder(targetStringLength);
+            for (int i = 0; i < transferForName; i++) {
+                int randomLimitedInt = leftLimit + (int)
+                        (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            generatedString = buffer.toString();
+
+        }
+
+        return generatedString;
+    }*/
 
